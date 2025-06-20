e2bec89300e1fe239a5536d30a20c56bff8944e6
Suryaraju3 <suryaraju485@gmail.com>
2025-06-20 12:40:18 +0530

    updated

--- py_oops.py
+++ py_oops.py
@@ -1,8 +1,19 @@
 
 
 #Ex:1
-class WhatsApp:
-    app="Whatsapp is a chatting app"
-wtsapp=WhatsApp()
-print(wtsapp.app)
+# class WhatsApp:
+#     app="Whatsapp is a chatting app"
+# wtsapp=WhatsApp()
+# print(wtsapp.app)
+
+#Ex:2
+#self ==>abc
+class Whats_app:
+    def __init__(abc,co_found,launchedyear):
+        abc.co_found=co_found
+        abc.lyear=launchedyear
+wtsapp2=Whats_app("Jan Koum and Brian Acton", 2009)
+print(wtsapp2.co_found)
+print(wtsapp2.lyear)
+
 
